b692db9fdc69bf4c1065709efe333e5364e979b2
Luc Bourlier <lb@skyluc.org>
2024-12-25 07:21:30 +0000

    Use ! to toggle band member details

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,23 +9,21 @@ function toggle_language_visibility(languageId) {
 }
 
 function toggle_band_member_details(member) {
-  var element = document.querySelectorAll("#member-details-" + member)[0]
-  element.classList.toggle("band_member_details_hidden")
+  document.querySelectorAll("#member-details-" + member)[0]
+    .classList.toggle("band_member_details_hidden")
+  document.querySelectorAll("#member-more-" + member)[0]
+    .classList.toggle("band_member_more_arrow_rotate")
+
 
   if (selected_band_member == "-") {
-    document.querySelectorAll("#member-more-" + member)[0].innerHTML = "⮙"
     selected_band_member = member
   } else if (selected_band_member == member) {
-    document.querySelectorAll("#member-more-" + member)[0].innerHTML = "⮛"
     selected_band_member = "-"
   } else {
-    document.querySelectorAll("#member-more-" + member)[0].innerHTML = "⮙"
-    
-    document.querySelectorAll("#member-more-" + selected_band_member)[0].innerHTML = "⮛"
-    var es = document.querySelectorAll("#member-details-" + selected_band_member)
-    es.forEach(element => {
-      element.classList.toggle("band_member_details_hidden")
-    })
+    document.querySelectorAll("#member-more-" + selected_band_member)[0]
+      .classList.toggle("band_member_more_arrow_rotate")
+    document.querySelectorAll("#member-details-" + selected_band_member)[0]
+      .classList.toggle("band_member_details_hidden")
 
     selected_band_member = member
   }

84996b87e425fefcdbf4ce477ea2c0554699da44
Luc Bourlier <lb@skyluc.org>
2025-04-25 15:37:24 +0000

    Differentiate docker instance name

--- tools/nginx/run.sh
+++ tools/nginx/run.sh
@@ -1,3 +1,3 @@
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../.." &> /dev/null && pwd )
 
-docker run -it --rm -d -p 8080:80 --name web -v "${ROOT_DIR}/target/site":/usr/share/nginx/html -v "${ROOT_DIR}/tools/nginx/conf.d":/etc/nginx/conf.d nginx
+docker run -it --rm -d -p 8080:80 --name webNEKI -v "${ROOT_DIR}/target/site":/usr/share/nginx/html -v "${ROOT_DIR}/tools/nginx/conf.d":/etc/nginx/conf.d nginx

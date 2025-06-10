56cf900c7e5d0d94130a275c78856573d81c868b
Luc Bourlier <lb@skyluc.org>
2025-06-10 07:52:39 +0000

    Adds covers in the linked-to for pages

--- neki-site/src/main/scala/org/skyluc/neki_site/checks/PopulateRelatedTo.scala
+++ neki-site/src/main/scala/org/skyluc/neki_site/checks/PopulateRelatedTo.scala
@@ -1,17 +1,18 @@
 package org.skyluc.neki_site.checks
 
+import org.skyluc.fan_resources.checks as fr
 import org.skyluc.fan_resources.data.{Data as _, Processor as _, *}
-import org.skyluc.fan_resources.{checks => fr}
 import org.skyluc.neki_site.data.*
 
 object PopulateRelatedTo extends fr.PopulateRelatedTo with Processor[Datum[?]] {
 
   override def processChronologyPage(chronologyPage: ChronologyPage): Datum[?] = {
-    chronologyPage
+    val references: Seq[Id[?]] = Seq(chronologyPage.coverImage.image)
+    chronologyPage.withLinkedTo(references*)
   }
 
   override def processMusicPage(musicPage: MusicPage): Datum[?] = {
-    val references: Seq[Id[?]] = musicPage.music
+    val references: Seq[Id[?]] = musicPage.music :+ musicPage.coverImage.image
 
     musicPage.withLinkedTo(references*)
   }
@@ -21,7 +22,7 @@ object PopulateRelatedTo extends fr.PopulateRelatedTo with Processor[Datum[?]] {
   }
 
   override def processShowsPage(showsPage: ShowsPage): Datum[?] = {
-    val references: Seq[Id[?]] = showsPage.shows
+    val references: Seq[Id[?]] = showsPage.shows :+ showsPage.coverImage.image
 
     showsPage.withLinkedTo(references*)
   }

2ce31c9ebea317405177039a9d3efda2d42a7b8e
Luc Bourlier <lb@skyluc.org>
2025-10-22 20:19:45 +0000

    Updates the front page

--- neki-site/src/main/scala/org/skyluc/neki_site/html/page/BandPage.scala
+++ neki-site/src/main/scala/org/skyluc/neki_site/html/page/BandPage.scala
@@ -38,61 +38,86 @@ class BandPage(site: Site, newsItems: Seq[fr.compileddata.NewsItemCompiledData])
             .withClass(CLASS_BAND_DESCRIPTION)
             .appendElements(
               p().appendElements(
-                text("NEK! is a young slang rock band formed in February 2024.")
+                text("NEK! is a young \"slang rock\" band formed in February 2024.")
               ),
               p().appendElements(
-                text("They are: Hika (vocals, guitar), Natsu (guitar), Kanade (bass) and Cocoro (Drums).")
+                text("The members are Hika (vocals, guitar), Natsu (guitar), Kanade (bass) and Cocoro (Drums).")
               ),
               p().appendElements(
-                text("They released their first single in April 2024, and their first EP in July 2024.")
+                text("They released their first single in April 2024, followed by their first EP in July 2024.")
               ),
               p().appendElements(
                 text(
-                  "Now in their second year, they released a second EP, did a 3 cities tour in Japan, and prepare for their first Zepp concert in August, at the Zepp Shinjuku in Tokyo."
+                  "Now in their second year, the band has released a second EP, completed a three-cities tour in Japan, and held their first Zepp concert in August at Zepp Shinjuku in Tokyo."
+                )
+              ),
+              p().appendElements(
+                text(
+                  "NEK! will release their first full album on November 26th, with a nation-wide tour already scheduled for early next year."
                 )
               ),
             ),
         )
         .appendElements(fr.component.NewsBlock.generate(newsItems)*),
-      bandPanel(),
+      bandPanel2(),
       div().withClass(CLASS_BAND_SOCIALS).appendElements(socials(site.band.socialMedia)*),
     )
   }
 
-  private def bandPanel(): Div = {
-    val elements = memberDivs("kanade", site.band.member.kanade) ::: memberDivs(
-      "hika",
-      site.band.member.hika,
-    ) ::: memberDivs("cocoro", site.band.member.cocoro) ::: memberDivs("natsu", site.band.member.natsu)
+  private def bandPanel2(): Div = {
+    val members =
+      Seq(
+        memberDivs2("kanade", site.band.member.kanade),
+        memberDivs2("hika", site.band.member.hika),
+        memberDivs2("cocoro", site.band.member.cocoro),
+        memberDivs2("natsu", site.band.member.natsu),
+      ).flatten
+    val central =
+      div()
+        .withClass(CLASS_BAND_PANEL2_CENTRAL)
+        .withClass(CLASS_BAND_PANEL2_SECTION)
+        .appendElements(
+          img().withSrc("/asset/image/site/band.png")
+        )
     div()
-      .withClass(CLASS_BAND_PANEL)
-      .appendElements(
-        elements*
-      )
+      .withClass(CLASS_BAND_PANEL2)
+      .appendElements(central)
+      .appendElements(members*)
   }
 
-  private def memberDivs(id: String, member: Member): List[Div] = {
-    List(
+  private def memberDivs2(id: String, member: Member): Seq[Div] = {
+    Seq(
       div()
-        .withClass(CLASS_BAND_PANEL_MEMBER_INFO)
-        .withClass(CLASS_BAND_PANEL_MEMBER_ + id)
+        .withClass(CLASS_BAND_PANEL2_MEMBER_ + id)
+        .withClass(CLASS_BAND_PANEL2_SECTION)
+        .appendElements(
+          img().withSrc("/asset/image/site/visual-" + id + ".png")
+        ),
+      div()
+        .withClass(CLASS_BAND_PANEL2_MEMBER_ + id)
+        .withClass(CLASS_BAND_PANEL2_MEMBER_INFO)
+        .withClass(CLASS_BAND_PANEL2_MEMBER_INFO_HIDDEN)
         .withOnClick(toggleClassCode(member.id))
         .appendElements(
           socials(member.socialMedia)*
         ),
       div()
-        .withClass(CLASS_BAND_PANEL_MEMBER_SUMMARY)
-        .withClass(CLASS_BAND_PANEL_MEMBER_ + id)
-        .withOnClick(toggleClassCode(member.id))
+        .withClass(CLASS_BAND_PANEL2_MEMBER_ + id)
+        .withClass(CLASS_BAND_PANEL2_MEMBER_SUMMARY)
         .appendElements(
-          div().withClass(CLASS_BAND_PANEL_MEMBER_SUMMARY_NAME).appendElements(text(member.name)),
-          div().withClass(CLASS_BAND_PANEL_MEMBER_SUMMARY_ROLE).appendElements(text(member.role)),
-        ),
+          div()
+            .withClass(CLASS_BAND_PANEL2_MEMBER_SUMMARY_NAME)
+            .appendElements(text(member.name)),
+          div()
+            .withClass(CLASS_BAND_PANEL2_MEMBER_SUMMARY_ROLE)
+            .appendElements(text(member.role)),
+        )
+        .withOnClick(toggleClassCode(member.id)),
     )
   }
 
   private def toggleClassCode(selector: String): String =
-    s"""toggleClass('$CLASS_BAND_PANEL_MEMBER_$selector','$CLASS_BAND_PANEL_MEMBER_SELECTED')"""
+    s"""toggleClass('$CLASS_BAND_PANEL2_MEMBER_$selector','$CLASS_BAND_PANEL2_MEMBER_INFO_HIDDEN')"""
 
   private def socials(socialMedia: SocialMedia): List[BodyElement[?]] = {
     List(
@@ -118,13 +143,15 @@ object BandPage {
   private val CLASS_BAND_NAME_SUBLABEL = "band-name-sublabel"
   private val CLASS_BAND_DESCRIPTION = "band-description"
 
-  private val CLASS_BAND_PANEL = "band-panel"
-  private val CLASS_BAND_PANEL_MEMBER_INFO = "band-panel-member-info"
-  private val CLASS_BAND_PANEL_MEMBER_SUMMARY = "band-panel-member-summary"
-  private val CLASS_BAND_PANEL_MEMBER_SUMMARY_NAME = "band-panel-member-summary-name"
-  private val CLASS_BAND_PANEL_MEMBER_SUMMARY_ROLE = "band-panel-member-summary-role"
-  private val CLASS_BAND_PANEL_MEMBER_ = "band-panel-member-"
-  private val CLASS_BAND_PANEL_MEMBER_SELECTED = "band-panel-member-selected"
+  private val CLASS_BAND_PANEL2 = "band-panel2"
+  private val CLASS_BAND_PANEL2_SECTION = "band-panel2-section"
+  private val CLASS_BAND_PANEL2_CENTRAL = "band-panel2-central"
+  private val CLASS_BAND_PANEL2_MEMBER_ = "band-panel2-member-"
+  private val CLASS_BAND_PANEL2_MEMBER_INFO_HIDDEN = "band-panel2-member-info-hidden"
+  private val CLASS_BAND_PANEL2_MEMBER_INFO = "band-panel2-member-info"
+  private val CLASS_BAND_PANEL2_MEMBER_SUMMARY = "band-panel2-member-summary"
+  private val CLASS_BAND_PANEL2_MEMBER_SUMMARY_NAME = "band-panel2-member-summary-name"
+  private val CLASS_BAND_PANEL2_MEMBER_SUMMARY_ROLE = "band-panel2-member-summary-role"
 
   private val CLASS_BAND_SOCIALS = "band-socials"
 
